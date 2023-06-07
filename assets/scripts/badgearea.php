b10a24c83b93649794d01fdd9e05862370ef47d0
Mar <just.mar.ok2813@gmail.com>
2023-06-07 14:49:25 +0000

    heheh

--- assets/scripts/badgearea.php
+++ assets/scripts/badgearea.php
@@ -6,8 +6,11 @@
     badge("https://yesterweb.org/no-to-web3/", "https://yesterweb.org/no-to-web3/img/roly-saynotoweb3.gif", "Crypto's ewie.", "badge saying 'Keep the web free, say no to web3'"),
     badge("https://minecraft.net/", "/assets/img/badges/minecraft.gif", "block game good", "minecraft"),
     badge("https://www.mozilla.org/nl/firefox/new/?redirect_source=firefox-com", "/assets/img/badges/getfirefox.gif", "GET FIREFOX!!", "Get Firefox"),
-    badge("","/assets/img/badges/fucknazis.gif","FUCK NAZI's!","Fuck nazis"),
+    badge("","/assets/img/badges/fucknazis.gif","FUCK NAZIS!","Fuck nazis"),
     badge("","/assets/img/badges/blinkiesCafe-autism.gif", "brain go brrr", "autism"),
+    badge("", "/assets/img/badges/blinkiesCafe-L1.gif", "AUTISM!", "autism"),
+    badge("", "/assets/img/badges/y2k-compliant.gif", "We survived! Or... well this site is from 2023. uhm.", "Y2K-compliant"),
+    badge("", "/assets/img/badges/transles80×31.png", "I'm trans, and girls", "trans-lesbian flag"),
     badge("https://www.tumblr.com/strawmelonjuice","/assets/img/badges/blinkiesCafe-tumblr-grrll.gif", "Tumblr Tumblr Tumblr", "Tumblr"),
     badge("", "/assets/img/badges/linux80x15.png", "team linux heheh", "Run linux"),
     badge("", "/assets/img/badges/feminism.gif", "intersectional feminism!", "feminism"),
@@ -16,9 +19,11 @@
     );
 
     shuffle($badges);
-
+    $maxbadges = 15;
     foreach ($badges as $badge) {
+        if ($count > $maxbadges) break;
         echo $badge;
+        $count = $count + 1;
 }
 ?>
 </div>
