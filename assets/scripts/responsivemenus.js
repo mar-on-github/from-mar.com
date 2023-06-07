9c6054709783133dfe466861d8057554933470d7
Mar <just.mar.ok2813@gmail.com>
2023-06-07 10:56:12 +0000

    sum enhancies

--- assets/scripts/responsivemenus.js
+++ assets/scripts/responsivemenus.js
@@ -27,11 +27,11 @@ function ToggleFilters() {
     console.log(document.body.style.filter)
     switch (document.body.style.filter) {
         default:
-            document.body.style.filter = "sepia(0.7)";
+            document.body.style.filter = "contrast(1.75)";
             document.body.style.backgroundColor = "#BD6F22";
             document.getElementById("filtertoggle").innerHTML = "Greyscale";
             break;
-        case ("sepia(0.7)"):
+        case ("contrast(1.75)"):
             document.body.style.filter = "grayscale(1)";
             document.body.style.backgroundColor = "#FFF0";
             document.getElementById("filtertoggle").innerHTML = "Colorful";
@@ -40,11 +40,11 @@ function ToggleFilters() {
         case ("grayscale(1)"):
             document.body.style.filter = "none";
             document.body.style.backgroundColor = "";
-            document.getElementById("filtertoggle").innerHTML = "Sepia";
+            document.getElementById("filtertoggle").innerHTML = "Contrast";
             break;
 }
 }
 
 document.body.style.filter = "none";
-document.getElementById("filtertoggle").innerHTML = "Sepia";
+document.getElementById("filtertoggle").innerHTML = "Contrast/Greyscale";
 document.getElementById("filtertoggle").style.padding = "10px";
