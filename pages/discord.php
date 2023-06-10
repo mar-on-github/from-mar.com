056df0d6d66e344185e726fd752ea64de3b6ca8c
Mar <just.mar.ok2813@gmail.com>
2023-06-10 15:06:09 +0200

    lmao

--- pages/discord.php
+++ pages/discord.php
@@ -8,14 +8,14 @@ echo(ReturnUniversalHeader("Blub!!", 'discord'));
 <body class="body">
     <button class="openbtn" onclick="openNav()">☰</button>
     <div class="sidebar" id="mySidebar"><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-    <p style="background-color: #738bd7; max-width: 300px; display: block; margin: 0px; text-align: center;"><img onclick="window.location.href='https://discord.gg/hHuKYXnGMU';" src="https://discord.com/api/guilds/853925881411141652/widget.png?style=banner2" alt="BLUB!!" width="300px" style="cursor: pointer"><small align="center">Discord Community</small></p>
+    <p style="background-color: #738bd7; max-width: 300px; display: block; margin: 0px; text-align: center;"><img onclick="window.location.href='https://discord.gg/hWXMRxGmyTV';" src="https://discord.com/api/guilds/853925881411141652/widget.png?style=banner2" alt="BLUB!!" width="300px" style="cursor: pointer"><small align="center">Discord Community</small></p>
         <?php echo(ReturnMenuLinksFromJSON("side","discord")) ?>
     </div>
     <div class="content" id="pagecontent" align="center">
 
         <h1>About</h1>
         <p>A community server and aspiring safe space, where everyone who respects others is welcomed!</p>
-        <button onclick="window.location.href='https://discord.gg/hHuKYXnGMU';" class="huge-button">Join!</button>
+        <button onclick="window.location.href='https://discord.gg/hWXMRxGmyTV';" class="huge-button">Join!</button>
     </div>
     <div class="bottombar" id="mybottombar">
         <?php echo(ReturnMenuLinksFromJSON("bottom","discord")) ?>
