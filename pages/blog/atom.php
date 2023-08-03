f57f62591a83539571bb4074dbd05a4bd1a04f2b
Mar <just.mar.ok2813@gmail.com>
2023-08-03 13:03:37 +0000

    png for consistency

--- pages/blog/atom.php
+++ pages/blog/atom.php
@@ -42,7 +42,7 @@ if (!$getsareset) {
     $bloglink = "https://{$_SERVER["HTTP_HOST"]}/blog?c=" . urlencode($filtercat);
     $blogname = "Stories by Mar 🤍 – $filtercat";
     if (($filtercat) == "Logger-Diary Online") {
-      $strawmelonjuiceimgurl = "https://logger-diary.strawmelonjuice.com/img/logo/logo_816px.webp";
+      $strawmelonjuiceimgurl = "https://logger-diary.strawmelonjuice.com/img/logo/logo_512px.png";
     }
   }
 }
